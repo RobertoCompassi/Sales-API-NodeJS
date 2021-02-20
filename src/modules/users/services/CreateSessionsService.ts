6ec7e6a39fd00f1cbea142e01a98b7ed8dd9ba37
Roberto Compassi <roberto.compassi@itravel.com.br>
2021-02-19 23:22:10 -0300

    Adicionado JWT e servico de sessao

---
+++ src/modules/users/services/CreateSessionsService.ts
@@ -0,0 +1,49 @@
+import { sign } from 'jsonwebtoken'
+
+import AppError from "@shared/errors/AppError";
+import { compare, hash } from "bcrypt";
+import { compile } from "joi";
+import { getCustomRepository } from "typeorm";
+import User from "../typeorm/entities/User";
+import { UsersRepository } from "../typeorm/repositories/UsersRepository";
+
+interface IRequest {
+    email: string;
+    password: string;
+}
+
+interface IResponse {
+    user: User;
+    token: string
+}
+
+class CreateSessionsService {
+    public async execute({ email, password }: IRequest) : Promise<IResponse> {
+        const usersRepository = getCustomRepository(UsersRepository);
+        
+        // ve se o email do usuario existe no banco
+        const user = await usersRepository.findByEmail(email);
+        if(!user){
+            throw new  AppError('Incorrect email/password combination.', 401);
+        }
+
+        // compara a senha que o user digitou com a criptografia
+        const passwordConfirmed = await compare(password, user.password);
+        if(!passwordConfirmed){
+            throw new  AppError('Incorrect email/password combination.', 401);
+        }
+
+        const token = sign({}, '008f6e57bf9191fbd69feb6fb34ab82b', {
+            subject: user.id,
+            expiresIn: '1d',
+        });
+
+        
+        return {
+            user,
+            token
+        };
+    }
+}
+
+export default CreateSessionsService
